3cf8cb22077a0e7a00e87ef528124cf418a90212
Liaqat Paindah <allneeda.paiendah@gmail.com>
2026-01-17 22:45:55 +0430

    country field revised

--- components/home-services/dashboard/profile-settings/edit/EditBasicInfo.tsx
+++ components/home-services/dashboard/profile-settings/edit/EditBasicInfo.tsx
@@ -632,7 +632,7 @@ export default function EditBasicInfo() {
                   </div>
                   <div>
                     <label className="text-sm font-medium text-gray-700 dark:text-gray-300 mb-1 block">
-                      Country <span className="text-red-500">*</span>
+                      Country <span className="text-red-500"></span>
                     </label>
                     <Input
                       {...register("country")}
